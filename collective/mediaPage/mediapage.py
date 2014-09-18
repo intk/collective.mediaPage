d7b2a6c4fd2d1441b3bb3f4b08e5ca0b0a1dd960
André Gonçalves <andreslb1@gmail.com>
2014-09-18 11:50:35 +0200

    Remove unnecessary files

--- collective/mediaPage/mediapage.py
+++ collective/mediaPage/mediapage.py
@@ -52,14 +52,13 @@ class mediaPage(dexterity.Container):
 # mediapage_templates.
 # Template filenames should be all lower case.
 # The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
+# interface with "/@@view" appended.
 # You may make this the default view for content objects
 # of this type by uncommenting the grok.name line below or by
 # changing the view class name and template filename to View / view.pt.
 
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(ImediaPage)
     grok.require('zope2.View')
 
     # grok.name('view')
-    
